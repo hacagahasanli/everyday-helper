7c1e1877c404ed8116864cacaad5e340fc7a0e44
hacagahasanli <hacagahasanli@gmail.com>
2026-01-16 16:18:38 +0400

    ...

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,33 +1,15 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: [
-    './src/index.ts',
-    // 'src/lib/index.ts',
-    // 'src/utils/index.ts',
-    // 'src/hooks/index.ts',
-    // 'src/constants/index.ts',
-  ],
+  entry: ['src/index.ts'],
   format: ['esm'],
-  dts: {
-    entry: 'src/index.ts',
-    resolve: true,
-    compilerOptions: {
-      moduleResolution: 'node10',
-      skipLibCheck: true,
-      allowSyntheticDefaultImports: true,
-      esModuleInterop: true,
-    },
-  },
-  clean: true,
-  // sourcemap: true,
-  minify: false, // keep readable for internal use
-  outDir: 'dist',
-  splitting: false,
-  keepNames: true,
-  treeshake: true,
+
+  // 🔑 THIS is where tsconfig goes
+  tsconfig: 'tsconfig.dts.json',
+
+  dts: true,
+
   external: ['react', 'react-dom', 'react-router-dom'],
-  onSuccess: 'copyfiles -u 1 "src/styles/**/*.css" dist',
-  ignoreWatch: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', 'src/__tests__'],
-  target: 'es2020',
+  clean: true,
+  target: 'es2020'
 });
